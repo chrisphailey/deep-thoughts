f010f603fd29dc026781725e36c5b4bed4f82beb
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-04-15 12:48:24 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+// use this to log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
